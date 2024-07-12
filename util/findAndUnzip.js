39fc935deb80246a51bf89090d40a7c55c4dcdd2
AlexDM0 <alexdemulder@gmail.com>
2024-07-12 11:16:05 +0200

    added pretty printing of time since download

--- util/findAndUnzip.js
+++ util/findAndUnzip.js
@@ -7,6 +7,7 @@ async function extractZip() {
   try {
     // Read the directory synchronously
     const files = fs.readdirSync(CONFIG.downloads);
+    const tmpDir = CONFIG.tmpFolderPath;
 
     // Filter and sort files synchronously
     const zipFiles = files.filter(file => CONFIG.firmwarePattern.test(file)).map(file => ({
@@ -21,7 +22,23 @@ async function extractZip() {
 
     const mostRecentFile = zipFiles[0].name;
     console.log('Found most recent firmware file:', mostRecentFile);
-    const tmpDir = CONFIG.tmpFolderPath;
+
+    // print how long ago this file was modified in seconds, minutes, or hours
+    const timeDiff = Date.now() - zipFiles[0].time;
+    const seconds = Math.floor(timeDiff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    if (minutes < 1) {
+      console.log('File was modified', seconds, 'seconds ago\n');
+    }
+    else if (hours < 1) {
+      console.log('File was modified', minutes, 'minutes ago\n');
+    }
+    else {
+      console.log('File was modified', hours, 'hours ago\n');
+    }
+
+
 
     // Delete the tmp directory if it exists
     if (fs.existsSync(tmpDir)) {
